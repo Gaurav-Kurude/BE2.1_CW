423020837120a1fd6418dc369feccd43d4972cc8
Gaurav-Kurude <gauravkurude.sitmech@gmail.com>
2026-06-24 15:52:54 +0530

    creating & Adding new data to the Database

---
+++ index.js
@@ -0,0 +1,32 @@
+const  {initializeDatabase} = require("./db/db.connect");
+
+const Movie = require("./models/movie.models");
+initializeDatabase();
+
+
+const newMovie = new Movie({
+  title: "New Movie",
+  releaseYear: 2023,
+  genre: ["Drama"],
+  director: "Aditya Roy Chopra",
+  actors: ["Actor1", "Actor2"],
+  language: "Hindi",
+  country: "India",
+  rating: 6.1,
+  plot: "A young man and woman fall in love on a London trip.",
+  awards: "IFA Filmfare Awards",
+  posterUrl: "https://example.com/new-poster1.jpg",
+  trailerUrl: "https://example.com/new-trailer1.mp4",
+});
+
+async function createMovie(newMovie) {
+    try {
+        const movie = new Movie(newMovie);
+        const saveMovie = await movie.save();
+        console.log(saveMovie);
+    }catch (error) {
+        throw error;
+    }
+}
+
+createMovie(newMovie);
